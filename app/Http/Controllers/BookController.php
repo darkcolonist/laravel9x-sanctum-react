fcddd6986e0f47cf6f67a523da5a1c2c20cb5f7f
cris <cris@nms.ph>
2022-08-15 10:23:38 +0800

    #4 cosmetic enhancements limiting book random data to 5 added loader

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,79 +7,83 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return response()->json(Book::inRandomOrder()->get());
-    }
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    // sleep(2);
+    $books = Book::inRandomOrder()
+      ->limit(5)->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Book $book)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-        //
-    }
+    return response()->json($books);
+  }
+  
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
+  
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    //
+  }
+  
+  /**
+  * Display the specified resource.
+  *
+  * @param  \App\Models\Book  $book
+  * @return \Illuminate\Http\Response
+  */
+  public function show(Book $book)
+  {
+    //
+  }
+  
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  \App\Models\Book  $book
+  * @return \Illuminate\Http\Response
+  */
+  public function edit(Book $book)
+  {
+    //
+  }
+  
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \App\Models\Book  $book
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, Book $book)
+  {
+    //
+  }
+  
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  \App\Models\Book  $book
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy(Book $book)
+  {
+    //
+  }
 }
--- resources/js/components/BooksSection.jsx
+++ resources/js/components/BooksSection.jsx
@@ -1,26 +1,33 @@
-import { Paper, Stack, Typography } from "@mui/material";
+import { LinearProgress, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 
 export default function(){
   const [books,setBooks] = React.useState([]);
+  const [booksLoaded,setBooksLoaded] = React.useState(false);
 
   React.useEffect(() => {
     axios.get('book')
       .then(response => {
         setBooks(response.data);
+        setBooksLoaded(true);
       });
   },[]);
 
   const bookRenderer = [];
 
-  books.forEach((book, index) => {
-    bookRenderer.push(
-      <Paper sx={{m:1, p:1}} key={index}>
-        <Typography variant="body1">{book.title}</Typography>
-        <Typography variant="body2">by {book.author}</Typography>
-      </Paper>
-    );
-  });
+  if(booksLoaded){
+    books.forEach((book, index) => {
+      bookRenderer.push(
+        <Paper sx={{m:1, p:1}} key={index}>
+          <Typography variant="body1">{book.title}</Typography>
+          <Typography variant="body2">by {book.author}</Typography>
+        </Paper>
+      );
+    });
+  }
+  else{
+    bookRenderer.push(<LinearProgress key="wait"/>);
+  }
 
   return <React.Fragment>
     <Typography variant="h5">Books</Typography>
