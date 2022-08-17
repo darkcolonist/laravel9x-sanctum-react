ac23cbee04f455d6e7cc73465bbd85f7a50fc017
cris <cris@nms.ph>
2022-08-17 10:20:20 +0800

    #5 sanctum implementation gracefully catching 401 and not making a big deal about it TODO additional permissions

--- resources/js/components/AxiosResponseInterceptor.jsx
+++ resources/js/components/AxiosResponseInterceptor.jsx
@@ -4,16 +4,24 @@
 import React from 'react';
 import { useAuthStore } from "./appState";
 
+function promiseResolveError(error){
+  // console.info('got axios error but handled:', error.response?.data?.message);
+  return Promise.resolve(error);
+}
+
 export default function(){
   const { setLoggedOut } = useAuthStore();
+  // const 
 
   React.useEffect(() => {
     axios.interceptors.response.use(undefined, function(error){
       switch(error.response.status){
         case 401:
           setLoggedOut(true);
-          break;
+          return promiseResolveError(error);
       }
+
+      return Promise.reject(error);
     });
   },[]);
 
